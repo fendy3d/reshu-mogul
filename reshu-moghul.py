e8dfae402d8f21b9f63315c2a4e6e92ca6a238d9
fendy3d <fendy3d@gmail.com>
2021-09-29 23:31:23 +0700

    Added postal code and contact  no

--- reshu-moghul.py
+++ reshu-moghul.py
@@ -1,10 +1,8 @@
 import pandas as pd
-import datetime
-from decimal import Decimal
 from fpdf import FPDF
 
 df = pd.read_csv("test.csv")
-series_uniqueOrderID = df["Order ID"].unique()
+series_uniqueOrderID = df["Order ID"].unique() #output: [1004,1005,1006]
 
 # create FPDF object
 # Layout ('P', 'L') -- portrait or landscape
@@ -12,18 +10,18 @@ series_uniqueOrderID = df["Order ID"].unique()
 # format ('A3','A4'(default),'A5','Letter','Legal',(100,150))
 pdf = FPDF('P')
 
-def concatenateAddress(address1, address2):
-	
+def concatenateAddress(address1, address2, postalcode):
 	if (pd.isna(address2)):
-		address1 = str(address1)
-		return address1
+		address1 = str(address1) 
+		return address1 + " " + postalcode
 	else: 
 		address1 = str(address1)
 		address2 = str(address2)
-		return address1 + " " + address2
+		return address1 + " " + address2 + " " + postalcode
 
 for orderID in series_uniqueOrderID:
 	name = ""
+	contact_number = ""
 	shipping_address = ""
 	delivery_datetime = ""
 	order_items = {}
@@ -31,9 +29,8 @@ for orderID in series_uniqueOrderID:
 	df_orderID = df[df["Order ID"] == orderID]
 
 	name = df_orderID["Full Name"].tolist()[0]
-
-	shipping_address = concatenateAddress(df_orderID["Shipping Address Line 1"].tolist()[0], df_orderID["Shipping Address Line 2"].tolist()[0])
-
+	contact_number = df_orderID["Contact"].tolist()[0]
+	shipping_address = concatenateAddress(df_orderID["Shipping Address Line 1"].tolist()[0], df_orderID["Shipping Address Line 2"].tolist()[0], str(df_orderID["Postal Code"].tolist()[0]))
 	delivery_datetime = df_orderID["Delivery Date"].tolist()[0]
 
 	for i in range(len(df_orderID["SKU"].tolist())):
@@ -49,7 +46,7 @@ for orderID in series_uniqueOrderID:
 	pdf.set_xy(x= x_position, y=12)
 	pdf.cell(200, 7, txt = "Order ID: " + str(orderID), ln = 1, align = 'L')
 	pdf.set_x(x= x_position)
-	pdf.cell(200, 7, txt = "Name: " + name, ln = 1, align = 'L')
+	pdf.cell(200, 7, txt = "Name: " + name + ", Contact: " + contact_number, ln = 1, align = 'L')
 	pdf.set_x(x= x_position)
 	pdf.cell(200, 7, txt = "Address: " + shipping_address, ln = 1, align = 'L')
 	pdf.set_x(x= x_position)
